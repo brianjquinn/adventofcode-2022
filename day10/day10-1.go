a8d7797f2a746e2f4674e5596bc500b76ee9f4d7
Brian Quinn <brianjeffreyquinn@gmail.com>
2022-12-10 00:45:14 -0800

    add day 10 setup

---
+++ day10/day10-1.go
@@ -0,0 +1,13 @@
+package day10
+
+import (
+	"fmt"
+
+	"github.com/brianjquinn/adventofcode-2022/utils"
+)
+
+func CathodeRayTubePart1() {
+	fmt.Println("Day 10 Part 1: Cathode-Ray Tube")
+
+	instructs := utils.ReadFileLinesToStringSlice("day10/cpu-instructions-ex.txt")
+}
---
+++ day10/day10-2.go
@@ -0,0 +1,13 @@
+package day10
+
+import (
+	"fmt"
+
+	"github.com/brianjquinn/adventofcode-2022/utils"
+)
+
+func CathodeRayTubePart2() {
+	fmt.Println("Day 10 Part 2: Cathode-Ray Tube")
+
+	instructs := utils.ReadFileLinesToStringSlice("day10/cpu-instructions-ex.txt")
+}
